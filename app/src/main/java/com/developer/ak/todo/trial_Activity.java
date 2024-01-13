9f32160ccbce64170d6256f1eb655db1abaedc09
Aditya <adityakarpe26@gmail.com>
2024-01-13 23:00:05 +0530

    TD

---
+++ app/src/main/java/com/developer/ak/todo/trial_Activity.java
@@ -0,0 +1,14 @@
+package com.developer.ak.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trial_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trial);
+    }
+}
